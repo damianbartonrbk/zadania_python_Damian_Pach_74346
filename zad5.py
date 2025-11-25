83cc400a8c1dbd669a058516065b735341370573
damianbartonrbk <74346@student.wsiz.edu.pl>
2025-11-25 18:39:43 +0100

    Delete zad5.py

--- zad5.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Podaj długość pierwszego boku prostokąta: "))
-b = float(input("Podaj długość drugiego boku prostokąta: "))
-pole = a * b
-obwod = a*2 + b*2
-print ("Pole prostokąta wynosi: ", pole)
-print ("Obwód prostokąta wynosi: ", obwod)
